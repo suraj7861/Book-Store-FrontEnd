c80a75ce975200dd6c53888b9b0058845dd5a5ea
unknown <khomanesuraj8@gmail.com>
2022-01-28 18:10:33 +0530

    Added Home Component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -12,4 +12,55 @@ export class HomePageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  bookList = [
+    {
+      'bookImages':'./assets/bookImages/Image1.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    },
+    {
+      'bookImages':'./assets/bookImages/Image1.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    },
+    {
+      'bookImages':'./assets/bookImages/Image1.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    },
+    {
+      'bookImages':'./assets/bookImages/Image4.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    },
+    {
+      'bookImages':'./assets/bookImages/Image5.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    },
+    {
+      'bookImages':'./assets/bookImages/Image6.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    },
+    {
+      'bookImages':'./assets/bookImages/Image7.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    },
+    {
+      'bookImages':'./assets/bookImages/Image8.png',
+      'bookName':'Don`t Make Me Think ',
+      'bookAuthor':'by Steve King',
+      'bookPrice':'1500',
+    }
+  ]
+
 }
